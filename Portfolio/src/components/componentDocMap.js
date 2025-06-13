19c8f2b25d220588621184eded5c402cf058b649
GirishEgo <Jai.girishego@gmail.com>
2025-06-13 13:55:46 +0530

    glassCardGuideData

---
+++ Portfolio/src/components/componentDocMap.js
@@ -0,0 +1,9 @@
+import glassCardGuideData from "./Props/PropsData/glassCardData";
+// import other component data files
+
+const componentDocMap = {
+    glasscard: glassCardGuideData,
+    // add more mappings here
+};
+
+export default componentDocMap;
