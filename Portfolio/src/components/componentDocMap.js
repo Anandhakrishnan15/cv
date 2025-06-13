50eb43c9eaab185fda6298000455e67e3fc94176
GirishEgo <Jai.girishego@gmail.com>
2025-06-13 14:41:08 +0530

    added contat form to test

--- Portfolio/src/components/componentDocMap.js
+++ Portfolio/src/components/componentDocMap.js
@@ -1,8 +1,12 @@
 import glassCardGuideData from "./Props/PropsData/glassCardData";
+import contatfromData from "./Props/PropsData/contatfromData";
+
 // import other component data files
 
 const componentDocMap = {
     glasscard: glassCardGuideData,
+    contact: contatfromData,
+    // gl
     // add more mappings here
 };
 
