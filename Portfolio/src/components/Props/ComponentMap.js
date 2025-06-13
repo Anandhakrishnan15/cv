f03558fa541f4eb5492faf1b6fde8e045700cac5
GirishEgo <Jai.girishego@gmail.com>
2025-06-13 14:43:20 +0530

    contat form compnets for reuse added  in cponent doc manp

---
+++ Portfolio/src/components/Props/ComponentMap.js
@@ -0,0 +1,12 @@
+// ./Props/ComponentMap.js
+import GlassCard from "./GlassCardDocs/GlassCard";
+// import Tooltip from "./Components/Tooltip";
+// import Button from "./Components/Button";
+import Contact from "../../pages/Contacts/Contact"
+const componentMap = {
+    GlassCard,
+    Contact,
+    // Button,
+};
+
+export default componentMap;
